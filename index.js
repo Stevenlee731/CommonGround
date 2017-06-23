92b9d18636a91f792066d642fbccd08020e5a7d1
stevecreated@gmail.com <stevecreated@gmail.com>
2017-06-22 23:19:19 -0700

    troubleshoot

--- index.js
+++ index.js
@@ -8,4 +8,8 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/src'));
 });
 
-app.listen(3000);
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});

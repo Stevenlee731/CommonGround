623d208bdb8448f51ffc918bdd7cd563a134f1ea
stevecreated@gmail.com <stevecreated@gmail.com>
2017-06-22 22:57:14 -0700

    troubleshooting server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 var express = require('express')
-var app = express()
-
-app.use(express.static('./src'))
+var server = express()
+server.use(express.static(__dirname + '/src'));
 
 app.listen(3000, () => {
   console.log('listening on port', 3000)

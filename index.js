43eac0ec0daae51a663e265f0fb5fbf94638f988
stevecreated@gmail.com <stevecreated@gmail.com>
2017-06-22 22:24:40 -0700

    added node

---
+++ index.js
@@ -0,0 +1,9 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
+
+app.use(express.static('src'))
+
+app.listen(3000, () => {
+  console.log('listening on port', 3000)
+})

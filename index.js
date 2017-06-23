2edc7d4549fe3ac67ee71d4e0efc846af5bc82b9
stevecreated@gmail.com <stevecreated@gmail.com>
2017-06-22 22:45:49 -0700

    removed bodyparser

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
 
-app.use(express.static('src'))
+app.use(express.static('./src'))
 
 app.listen(3000, () => {
   console.log('listening on port', 3000)
